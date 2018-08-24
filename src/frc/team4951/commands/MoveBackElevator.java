02bcc43720563e97b1bf4d43f5a9c78c47c92071
CarterMoore <cartermoore8@gmail.com>
2018-08-23 20:35:42 -0700

    Back Elevator subsystem

--- src/frc/team4951/commands/MoveBackElevator.java
+++ src/frc/team4951/commands/MoveBackElevator.java
@@ -1,4 +1,25 @@
 package frc.team4951.commands;
 
-public class MoveBackElevator {
+import frc.team4951.OI;
+
+public class MoveBackElevator extends CommandBase {
+
+    private static final double MOTOR_SPEED = 0.8;
+    private static final double DEADZONE = 0.5;
+
+    @Override
+    protected void execute() {
+        if (OI.getOperatorRightY() >= DEADZONE)
+            backElevator.moveElevator(MOTOR_SPEED);
+        else if (OI.getOperatorRightY() <= -DEADZONE)
+            backElevator.moveElevator(-MOTOR_SPEED);
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return Math.abs(OI.getOperatorRightY()) < DEADZONE;
+    }
+
+    @Override
+    protected void end() {backElevator.stop();}
 }
