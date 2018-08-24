73e3e0690dc86814046bda03fc245c1f68de9802
CarterMoore <cartermoore8@gmail.com>
2018-08-23 20:36:34 -0700

    Back elevator subsystem

--- src/frc/team4951/subsystems/BackElevator.java
+++ src/frc/team4951/subsystems/BackElevator.java
@@ -1,17 +1,31 @@
 package frc.team4951.subsystems;
 
 
+import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class BackElevator extends Subsystem {
 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+    private static final int BACK_ELEVATOR_MOTOR = 0;
 
+    private static BackElevator instance;
 
-    public void initDefaultCommand() {
-        // TODO: Set the default command, if any, for a subsystem here. Example:
-        //    setDefaultCommand(new MySpecialCommand());
+    public static BackElevator getInstance() {
+        if (instance == null)
+            instance = new BackElevator();
+        return instance;
     }
+
+    private Spark backMotor;
+
+    private BackElevator() {
+        backMotor = new Spark(BACK_ELEVATOR_MOTOR);
+    }
+
+    public void moveElevator(double speed) {backMotor.set(speed);}
+
+    public void stop() {backMotor.set(0);}
+
+    public void initDefaultCommand() {}
 }
 
