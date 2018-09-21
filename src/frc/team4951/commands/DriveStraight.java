7761536e8b839e067ce3ffcd09ff01cff75b2527
CarterMoore <cartermoore8@gmail.com>
2018-09-21 12:32:59 -0400

    Added commands for driving straight and turning during autonomous

--- src/frc/team4951/commands/DriveStraight.java
+++ src/frc/team4951/commands/DriveStraight.java
@@ -1,8 +1,5 @@
 package frc.team4951.commands;
 
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.PIDController;
-import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  * 2018Robot
@@ -10,23 +7,28 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  * Version 1.0
  */
 public class DriveStraight extends CommandBase {
-
-    private static final double kP = 0.01, kI = 0.0, kD = 0.0;
-    
-    private PIDController drivePID;
-    private PIDController rotationPID;
-    
-    private Encoder encoder;
     
     private double targetDistance;
     
     public DriveStraight(double distance) {
         requires(driveTrain);
         targetDistance = distance;
+        driveTrain.reset();
     }
     
     @Override
     protected void execute () {
-    
+        driveTrain.setSetpoint(targetDistance);
+        driveTrain.enable();
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return driveTrain.onTarget();
+    }
+
+    @Override
+    protected void end() {
+        driveTrain.stop();
     }
 }
